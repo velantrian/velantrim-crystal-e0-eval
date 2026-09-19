65832d440199d9224f513ae1c7c4b325185cb669
Velantim <velantrim@gmail.com>
2026-09-19 17:35:28 +0200

    feat(e0): add crystal_eval/__init__.py

---
+++ crystal_eval/__init__.py
@@ -0,0 +1,5 @@
+"""External E0 evaluation harness for Velantrim Exo-Cortex Crystal."""
+
+CRYSTAL_REPO = "velantrian/velantrim-exocortex-crystal"
+CRYSTAL_SHA = "3ed3a53ee9b8445a5f2ebf16046f782a3095555e"
+E0_VERSION = "0.1.0"
